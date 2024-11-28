b5fdda774c27460eb0867f529a28920ff3a72955
rozin.dmitriy2 <rozin.dmitriy2@wb.ru>
2024-11-28 14:07:26 +0300

    address upd

--- back/back.go
+++ back/back.go
@@ -6,8 +6,14 @@ import (
 	"log"
 	"messenger/front/handlers"
 	"net/http"
+	"runtime"
 	"strconv"
 	"sync"
+)
+
+const (
+	localHost = "localhost:"
+	ip        = "127.0.0.1:"
 )
 
 func Start(ctx context.Context, wg *sync.WaitGroup) {
@@ -34,8 +40,15 @@ func startFront(port int, errCh chan error) {
 	log.Println("Starting HTTP Server")
 	log.Printf("Listening on port %d", port)
 
+	ipStarter := ""
+	if runtime.GOOS == "linux" {
+		ipStarter = ip
+	} else {
+		ipStarter = localHost
+	}
+
 	server := &http.Server{
-		Addr: "localhost:" + strconv.Itoa(port),
+		Addr: ipStarter + strconv.Itoa(port),
 	}
 	serverIsRunning := make(chan bool)
 	go func(serverIsRunning chan bool) {
